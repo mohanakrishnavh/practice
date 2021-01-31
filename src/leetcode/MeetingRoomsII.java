967adcab1faf1dbf9b7b6e914b0ffccb7d4b932a
Mohanakrishna <mohanakrishnavh@gmail.com>
2021-01-31 18:32:07 -0500

    Create MeetingRoomsII.java

---
+++ src/leetcode/MeetingRoomsII.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class MeetingRoomsII {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<int[]> minHeap= new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        minHeap.add(intervals[0]);
+
+        for (int i=1; i<intervals.length; i++) {
+            int[] current = intervals[i];
+            int[] earliest = minHeap.remove();
+
+            if (current[0] >= earliest[1]) {
+                earliest[1] = current[1];
+            } else {
+                minHeap.add(current);
+            }
+
+            minHeap.add(earliest);
+        }
+
+        return minHeap.size();
+    }
+}
