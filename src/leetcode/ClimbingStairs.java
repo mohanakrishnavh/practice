80072cb0c27d0790418f100ef7d78d640aa3e310
Mohanakrishna <mohanakrishnavh@gmail.com>
2021-02-14 19:54:40 -0500

    Climbing stairs

---
+++ src/leetcode/ClimbingStairs.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        if (n == 1 || n == 2) {
+            return n;
+        }
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
