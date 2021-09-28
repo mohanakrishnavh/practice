d6c7ec18c53faf8ba244371ffddbae51cb33b46d
Mohanakrishna <mohanakrishnavh@gmail.com>
2021-09-28 11:35:38 -0700

    2nd method ngr

--- src/dsa/stack/NearestGreaterToRight.java
+++ src/dsa/stack/NearestGreaterToRight.java
@@ -28,8 +28,32 @@ public class NearestGreaterToRight {
         return ngr;
     }
 
+    public static int[] getNearestGreaterToRight2(int[] nums) {
+        int[] nextLargestElements = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=nums.length-1; i>=0; i--) {
+            if (stack.isEmpty()) {
+              nextLargestElements[i] = -1;
+            } else if (stack.peek() > nums[i]) {
+                nextLargestElements[i] = stack.peek();
+            } else {
+                while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                    stack.pop();
+                }
+
+                nextLargestElements[i] = stack.isEmpty() ? -1 : stack.peek();
+            }
+
+            stack.push(nums[i]);
+        }
+
+        return nextLargestElements;
+    }
+
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 3, 0, 0, 1, 2, 4};
-        System.out.println(Arrays.toString(NearestGreaterToRight.getNearestGreaterToRight(nums)));
+        System.out.println(Arrays.toString(NearestGreaterToRight.getNearestGreaterToRight2(nums)));
     }
 }
