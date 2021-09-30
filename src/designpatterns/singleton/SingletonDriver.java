d9a35d23be77437500ad7db3d72a1c50f92c1eae
Mohanakrishna <mohanakrishnavh@gmail.com>
2021-09-29 20:08:49 -0700

    Singleton - Thread calls

--- src/designpatterns/singleton/SingletonDriver.java
+++ src/designpatterns/singleton/SingletonDriver.java
@@ -2,10 +2,17 @@ package designpatterns.singleton;
 
 public class SingletonDriver {
     public static void main(String[] args) {
-        SingletonBlockSynchronized instance1 = SingletonBlockSynchronized.getInstance();
-        System.out.println(instance1);
+        Thread t1 = new Thread(() -> {
+            SingletonBlockSynchronized instance1 = SingletonBlockSynchronized.getInstance();
+            System.out.println(instance1);
+        });
 
-        SingletonBlockSynchronized instance2 = SingletonBlockSynchronized.getInstance();
-        System.out.println(instance2);
+        Thread t2 = new Thread(() -> {
+            SingletonBlockSynchronized instance2 = SingletonBlockSynchronized.getInstance();
+            System.out.println(instance2);
+        });
+
+        t1.start();
+        t2.start();
     }
 }
