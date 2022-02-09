55346af308412b0ae00ec968b1d4f85538de03c3
Mohanakrishna <mohanakrishnavh@gmail.com>
2022-02-08 20:41:30 -0800

    Bubble Sort

--- src/dsa/sort/BubbleSort.java
+++ src/dsa/sort/BubbleSort.java
@@ -1,4 +1,31 @@
 package dsa.sort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void sort(int[] A) {
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // In a pass if no elements are swapped, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[]{2, 7, 4, 1, 5, 3};
+        BubbleSort.sort(A);
+        System.out.println(Arrays.toString(A));
+    }
 }
--- src/dsa/sort/SelectionSort.java
+++ src/dsa/sort/SelectionSort.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void sort(int[] A) {
-        for (int i = 0; i < A.length - 1; i++) {
+        int n = A.length;
+        for (int i = 0; i < n-1; i++) {
             int minIndex = i;
-            for (int j = i + 1; j < A.length; j++) {
+            for (int j = i + 1; j < n; j++) {
                 if (A[j] < A[minIndex]) {
                     minIndex = j;
                 }
