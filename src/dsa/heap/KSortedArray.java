fed4078e199b2ae2023bbea43ac00b06f132524a
Mohanakrishna <mohanakrishnavh@gmail.com>
2022-02-13 18:33:54 -0800

    k sorted array

--- src/dsa/heap/KSortedArray.java
+++ src/dsa/heap/KSortedArray.java
@@ -1,41 +1,61 @@
 package dsa.heap;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class KSortedArray {
-
-    /**
-     * Sorts a k-sorted array
-     * <p>
-     * Time Complexity : O(n log(k)) - n is the size of the array
-     * Space Complexity : O(k) - k is the size of the heap
-     */
     public static int[] sort(int[] nums, int k) {
         if (nums.length == 0 || k <= 0) {
             return null;
         }
 
         int[] sortedNums = new int[nums.length];
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
         int index = 0;
-        for (int i : nums) {
-            minHeap.add(i);
+        for (int num : nums) {
+            priorityQueue.add(num);
 
-            if (minHeap.size() > k) {
-                sortedNums[index++] = minHeap.poll();
+            if (priorityQueue.size() > k) {
+                sortedNums[index++] = priorityQueue.poll();
             }
         }
 
-        while (minHeap.size() > 0) {
-            sortedNums[index++] = minHeap.poll();
+        while (!priorityQueue.isEmpty()) {
+            sortedNums[index++] = priorityQueue.poll();
         }
 
         return sortedNums;
     }
 
+    public static int[] sortInDescendingOrder(int[] nums, int k) {
+        if (nums.length == 0 || k <= 0) {
+            return null;
+        }
+
+        int[] sortedNums = new int[nums.length];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        int index = 0;
+        for (int i= nums.length-1; i>=0; i--) {
+            priorityQueue.add(nums[i]);
+
+            if (priorityQueue.size() > k) {
+                sortedNums[index++] = priorityQueue.poll();
+            }
+        }
+
+        while (!priorityQueue.isEmpty()) {
+            sortedNums[index++] = priorityQueue.poll();
+        }
+
+        return sortedNums;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(KSortedArray.sort(new int[]{6, 5, 3, 2, 8, 10, 9}, 3)));
+        System.out.println(Arrays.toString(sort(new int[]{6, 5, 3, 2, 8, 10, 9}, 3)));
+        System.out.println(Arrays.toString(sortInDescendingOrder(new int[]{6, 5, 3, 2, 8, 10, 9}, 3)));
     }
 }
