03eeb3005945fbf2e996951401122e534d9aaf86
Mohanakrishna <mohanakrishnavh@gmail.com>
2022-03-02 15:15:13 -0800

    ngl code cleanup

--- src/dsa/stack/NearestGreaterToLeft.java
+++ src/dsa/stack/NearestGreaterToLeft.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class NearestGreaterToLeft {
-    private static int[] getNearestGreaterToLeft(int[] nums) {
+
+    /*private static int[] getNearestGreaterToLeft(int[] nums) {
         int[] ngl = new int[nums.length];
         Stack<Integer> stack = new Stack<>();
 
@@ -25,7 +26,26 @@ public class NearestGreaterToLeft {
         }
 
         return ngl;
+    }*/
+
+    private static int[] getNearestGreaterToLeft(int[] nums) {
+        int[] ngl = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                    stack.pop();
+                }
+            }
+
+            ngl[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i]);
+        }
+
+        return ngl;
     }
+
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 3, 2, 4};
