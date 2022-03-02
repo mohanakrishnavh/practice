44d401d1dba9cfb656ae340be32675cebbdf0c65
Mohanakrishna <mohanakrishnavh@gmail.com>
2022-03-02 15:12:29 -0800

    ngr code comment

--- src/dsa/stack/NearestGreaterToRight.java
+++ src/dsa/stack/NearestGreaterToRight.java
@@ -4,6 +4,30 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class NearestGreaterToRight {
+
+    /*private static int[] getNearestGreaterToRight(int[] nums) {
+        int[] ngr = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (stack.isEmpty()) {
+                ngr[i] = -1;
+            } else if (stack.peek() > nums[i]) {
+                ngr[i] = stack.peek();
+            } else {
+                while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                    stack.pop();
+                }
+
+                ngr[i] = stack.isEmpty() ? -1 : stack.peek();
+            }
+
+            stack.push(nums[i]);
+        }
+
+        return ngr;
+    }*/
+
     private static int[] getNearestGreaterToRight(int[] nums) {
         int[] ngr = new int[nums.length];
         Stack<Integer> stack = new Stack<>();
