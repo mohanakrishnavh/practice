f9200bec86de8609254f5b2c6b1638b0f489b718
Mohanakrishna <mohanakrishnavh@gmail.com>
2022-03-13 20:50:01 -0700

    Peak Element

--- src/leetcode/LC0162_FindPeakElement.java
+++ src/leetcode/LC0162_FindPeakElement.java
@@ -1,7 +1,7 @@
-package dsa.search;
+package leetcode;
 
-public class PeakElement {
-    public static int getPeakElement(int[] nums) {
+public class LC0162_FindPeakElement {
+    public int findPeakElement(int[] nums) {
         int size = nums.length;
         int start = 0;
         int end = size - 1;
@@ -31,15 +31,5 @@ public class PeakElement {
         }
 
         return -1;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(getPeakElement(new int[]{5, 10, 20, 15}));
-        System.out.println(getPeakElement(new int[]{10, 20, 15, 2, 23, 90, 67}));
-        System.out.println(getPeakElement(new int[]{10, 20, 30, 40, 50}));
-        System.out.println(getPeakElement(new int[]{50, 40, 30, 20, 10}));
-        System.out.println(getPeakElement(new int[]{1}));
-        System.out.println(getPeakElement(new int[]{1, 2}));
-        System.out.println(getPeakElement(new int[]{3, 4, 3, 2, 1}));
     }
 }
