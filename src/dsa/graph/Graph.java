1d7d56ffeefe5029bc1938bb2f809ef52ad1fd46
Mohanakrishna <mohanakrishnavh@gmail.com>
2022-03-19 11:31:17 -0700

    graph - bfs & dfs

--- src/dsa/graph/Graph.java
+++ src/dsa/graph/Graph.java
@@ -9,13 +9,21 @@ import java.util.Stack;
 public class Graph {
     List<List<Integer>> adjacacencyList;
     boolean[] visited;
+    int size;
 
     public Graph(int size) {
-        adjacacencyList = new ArrayList<>();
-        visited = new boolean[size];
+        this.size = size;
+        this.adjacacencyList = new ArrayList<>();
+        this.visited = new boolean[this.size];
 
         for (int i = 0; i < size; i++) {
             adjacacencyList.add(i, new ArrayList<>());
+        }
+    }
+
+    public void addUndirectedEdges(int[][] edgeList) {
+        for (int[] edge : edgeList) {
+            addUndirectedEdge(edge[0], edge[1]);
         }
     }
 
@@ -24,53 +32,70 @@ public class Graph {
         adjacacencyList.get(b).add(a);
     }
 
+    public void addDirectedEdges(int[][] edgeList) {
+        for (int[] edge : edgeList) {
+            addDirectedEdge(edge[0], edge[1]);
+        }
+    }
+
     public void addDirectedEdge(int a, int b) {
         adjacacencyList.get(a).add(b);
     }
 
-    public List<Integer> breadthFirstTraversal(int startIndex) {
-        List<Integer> bfsTraversal = new ArrayList<>();
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(startIndex);
-        visited[startIndex] = true;
-        bfsTraversal.add(startIndex);
-
-        while (!queue.isEmpty()) {
-            Integer node = queue.poll();
-            List<Integer> children = adjacacencyList.get(node);
-
-            for (Integer child : children) {
-                if (!visited[child]) {
-                    queue.add(child);
-                    visited[child] = true;
-                    bfsTraversal.add(child);
-                }
-            }
-        }
-
-        return bfsTraversal;
+    public void refreshVisited() {
+        this.visited = new boolean[this.size];
     }
 
-    public List<Integer> depthFirstTraversal(int startIndex) {
-        List<Integer> dfsTraversal = new ArrayList<>();
-
+    public List<Integer> depthFirstTraversal(int start) {
+        List<Integer> traversal = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
-        stack.push(startIndex);
-        visited[startIndex] = true;
+        stack.push(start);
 
         while (!stack.isEmpty()) {
+            // Pop the vertex from the stack
             Integer node = stack.pop();
-            dfsTraversal.add(node);
 
+            // Visit the unvisited vertex, add it to output
+            if (!visited[node]) {
+                visited[node] = true;
+                traversal.add(node);
+            }
+
+            // View the adjacency list and add the nodes that have not been visited to the stack
             List<Integer> children = adjacacencyList.get(node);
             for (Integer child : children) {
                 if (!visited[child]) {
                     stack.push(child);
-                    visited[child] = true;
                 }
             }
         }
 
-        return dfsTraversal;
+        return traversal;
+    }
+
+    public List<Integer> breadthFirstTraversal(int start) {
+        List<Integer> traversal = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            // Deque the node from the queue and add it output
+            Integer node = queue.poll();
+
+            // Visit the unvisited vertex, add it to output
+            if (!visited[node]) {
+                visited[node] = true;
+                traversal.add(node);
+            }
+
+            List<Integer> children = adjacacencyList.get(node);
+            for (Integer child : children) {
+                if (!visited[child]) {
+                    queue.add(child);
+                }
+            }
+        }
+
+        return traversal;
     }
 }
--- src/dsa/graph/GraphTraversalDriver.java
+++ src/dsa/graph/GraphTraversalDriver.java
@@ -2,25 +2,22 @@ package dsa.graph;
 
 public class GraphTraversalDriver {
     public static void main(String[] args) {
-        Graph g1 = new Graph(6);
-        g1.addUndirectedEdge(0, 1);
-        g1.addUndirectedEdge(0, 2);
-        g1.addUndirectedEdge(0, 3);
-        g1.addUndirectedEdge(1, 4);
-        g1.addUndirectedEdge(2, 4);
-        g1.addUndirectedEdge(2, 5);
-        g1.addUndirectedEdge(4, 5);
+        Graph graph1 = new Graph(7);
+        graph1.addUndirectedEdges(new int[][]{{0, 1}, {0, 2}, {1, 3}, {3, 5}, {2, 4}, {4, 5}, {4, 6}});
 
-        System.out.println("Breadth First Traversal : " + g1.breadthFirstTraversal(0));
+        Graph graph2 = new Graph(7);
+        graph2.addDirectedEdges(new int[][]{{0, 1}, {0, 2}, {1, 3}, {3, 5}, {2, 4}, {4, 5}, {4, 6}});
 
-        Graph g2 = new Graph(7);
-        g2.addDirectedEdge(0, 1);
-        g2.addDirectedEdge(0, 2);
-        g2.addDirectedEdge(1, 3);
-        g2.addDirectedEdge(3, 5);
-        g2.addDirectedEdge(2, 4);
-        g2.addDirectedEdge(4, 5);
-        g2.addDirectedEdge(4, 6);
-        System.out.println("Depth First Traversal : " + g2.depthFirstTraversal(0));
+        // Depth First Traversal
+        System.out.println("Depth First Traversal (Undirected Graph) : " + graph1.depthFirstTraversal(0));
+        graph1.refreshVisited();
+        System.out.println("Depth First Traversal (Directed Graph) : " + graph2.depthFirstTraversal(0));
+        graph2.refreshVisited();
+
+        // Breadth First Traversal
+        System.out.println("Breadth First Traversal (Undirected Graph) : " + graph1.breadthFirstTraversal(0));
+        graph1.refreshVisited();
+        System.out.println("Breadth First Traversal (Directed Graph) : " + graph2.breadthFirstTraversal(0));
+        graph2.refreshVisited();
     }
 }
