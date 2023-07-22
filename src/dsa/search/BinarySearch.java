8c545928f00c168d85fdfb7458acce39a4e0d6ac
Mohanakrishna <mohanakrishnavh@gmail.com>
2023-07-21 19:37:06 -0700

    Binary Search Recursive Implementation

--- src/dsa/search/BinarySearch.java
+++ src/dsa/search/BinarySearch.java
@@ -1,25 +1,46 @@
 package dsa.search;
 
 public class BinarySearch {
-    public static int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+    public static int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            if (target == nums[mid]) {
+            if (target == arr[mid]) {
                 return mid;
             }
 
-            if (target < nums[mid]) {
-                end = mid - 1;
+            if (target < arr[mid]) {
+                high = mid - 1;
             } else {
-                start = mid + 1;
+                low = mid + 1;
             }
         }
 
         return -1;
+    }
+
+    public static int searchRecursive(int[] arr, int target) {
+        return searchRecursive(arr, 0, arr.length - 1, target);
+    }
+
+    public static int searchRecursive(int[] arr, int low, int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + (high - low)/2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (target > arr[mid]) {
+            BinarySearch.searchRecursive(arr, mid + 1, high, target);
+        }
+
+        return BinarySearch.searchRecursive(arr, 0, mid - 1, target);
     }
 
     public static int search(int[] nums, int startIndex, int endIndex, int target) {
@@ -46,5 +67,7 @@ public class BinarySearch {
     public static void main(String[] args) {
         System.out.println(search(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5));
         System.out.println(search(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 100));
+        System.out.println(searchRecursive(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5));
+        System.out.println(searchRecursive(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 100));
     }
 }
