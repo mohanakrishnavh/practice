eaa333effdb139fcfda5bc83c383542bb1a0590e
Mohanakrishna <mohanakrishnavh@gmail.com>
2023-07-22 21:28:05 -0700

    Find Peak Element

--- src/leetcode/LC0162_FindPeakElement.java
+++ src/leetcode/LC0162_FindPeakElement.java
@@ -32,4 +32,42 @@ public class LC0162_FindPeakElement {
 
         return -1;
     }
+
+    public static int findPeakElement2(int[] arr) {
+        int n = arr.length;
+
+        // If there is only one element in the array
+        if (n == 1) {
+            return 0;
+        }
+
+        // If the first element is the peak element
+        if (arr[0] > arr[1]) {
+            return 0;
+        }
+
+        // If the last element is the peak element
+        if (arr[n-1] > arr[n-2]) {
+            return n-1;
+        }
+
+        int low = 1, high = n-2;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+
+            // Peak Element
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            // If left half is the sorted half
+            if (arr[mid - 1] < arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
 }
