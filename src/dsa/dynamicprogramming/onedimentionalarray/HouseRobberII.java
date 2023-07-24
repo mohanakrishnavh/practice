33c6781c2168cf14b201edda5cec28a7fcda4ae0
Mohanakrishna <mohanakrishnavh@gmail.com>
2023-07-23 23:13:23 -0700

    Update HouseRobberII.java

--- src/dsa/dynamicprogramming/onedimentionalarray/HouseRobberII.java
+++ src/dsa/dynamicprogramming/onedimentionalarray/HouseRobberII.java
@@ -1,31 +1,35 @@
 package dsa.dynamicprogramming.onedimentionalarray;
 
 public class HouseRobberII {
-    public int houseRobber2(int[] nums) {
-        int n = nums.length;
-        int[] excludeFirst = new int[n-1];
-        int[] excludeLast = new int[n-1];
+    public static int rob(int[] arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        }
 
-        int first = 0, second = 0;
-        for (int i = 0; i < n; i++) {
-            if (i != 0) {
-                excludeFirst[first++] = nums[i];
-            }
+        int[] excludingFirst = excludeIndex(arr, 0);
+        int[] excludingLast = excludeIndex(arr, arr.length - 1);
 
-            if (i != nums.length-1) {
-                excludeLast[second++] = nums[i];
+        return Math.max(robHelper(excludingFirst), robHelper(excludingLast));
+    }
+
+    private static int[] excludeIndex(int[] arr, int excludeIndex) {
+        int[] excludedArr = new int[arr.length - 1];
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != excludeIndex) {
+                excludedArr[idx++] = arr[i];
             }
         }
 
-        return Math.max(rob2(excludeFirst), rob2(excludeLast));
+        return excludedArr;
     }
 
-    private int rob2(int[] nums) {
-        int n = nums.length;
+    private static int robHelper(int[] arr) {
+        int n = arr.length;
 
-        int prev = nums[0], prev2 = 0;
+        int prev = arr[0], prev2 = 0;
         for (int i = 1; i < n; i++) {
-            int take = i > 1 ? nums[i] + prev2 : nums[i];
+            int take = i > 1 ? arr[i] + prev2 : arr[i];
             int notTake = prev;
 
             int result = Math.max(take, notTake);
@@ -36,6 +40,4 @@ public class HouseRobberII {
 
         return prev;
     }
-
-
 }
