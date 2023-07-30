72bc98d54850cae37611b24e75a2dcdf1c25a43a
Mohanakrishna <mohanakrishnavh@gmail.com>
2023-07-30 13:55:52 -0700

    getTopKFrequentNumbers

--- src/dsa/heap/TopKFrequentNumbers.java
+++ src/dsa/heap/TopKFrequentNumbers.java
@@ -5,35 +5,29 @@ import dsa.util.FrequencyUtil;
 import java.util.*;
 
 public class TopKFrequentNumbers {
-    public static List<Integer> getTopKFrequentNumbers(int[] nums, int k) {
-        List<Integer> topKFrequentNumbers = new ArrayList<>();
-        if (nums == null || nums.length <= 1 || k == 0) {
-            return topKFrequentNumbers;
-        }
+    public static int[] getTopKFrequentNumbers(int[] nums, int k) {
+        int[] topKFrequentNumbers = new int[k];
 
         Map<Integer, Integer> countMap = FrequencyUtil.getCountMap(nums);
 
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            int[] frequencyNumberPair = new int[]{entry.getValue(), entry.getKey()};
-            if (minHeap.size() < k) {
-                minHeap.add(frequencyNumberPair);
-            } else {
-                if (frequencyNumberPair[0] > minHeap.peek()[0]) {
-                    minHeap.offer(frequencyNumberPair);
-                    minHeap.poll();
-                }
+            minHeap.add(new int[]{entry.getValue(), entry.getKey()});
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
             }
         }
 
-        while (!minHeap.isEmpty()) {
-            topKFrequentNumbers.add(minHeap.poll()[1]);
+        int idx = 0;
+        while(!minHeap.isEmpty()) {
+            topKFrequentNumbers[idx++] = minHeap.poll()[1];
         }
 
         return topKFrequentNumbers;
     }
 
     public static void main(String[] args) {
-        System.out.println(getTopKFrequentNumbers(new int[]{1, 1, 1, 3, 2, 2, 4}, 2));
+        System.out.println(Arrays.toString(getTopKFrequentNumbers(new int[]{1, 1, 1, 3, 2, 2, 4}, 2)));
     }
 }
