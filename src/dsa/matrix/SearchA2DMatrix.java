dea4d06f1582c9591ea877c971a219fcf38fe806
Mohanakrishna <mohanakrishnavh@gmail.com>
2023-09-12 12:23:58 -0700

    Matrix Problems

---
+++ src/dsa/matrix/SearchA2DMatrix.java
@@ -0,0 +1,47 @@
+package dsa.matrix;
+
+public class SearchA2DMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[i].length - 1;
+
+        // Find the row in which the element can exist
+        while (i >= 0 && i <= matrix.length - 1) {
+            if (matrix[i][0] <= target && target <= matrix[i][j]) {
+                break;
+            }
+
+            i++;
+        }
+
+        // If the element is not present in the matrix
+        if (i == matrix.length) {
+            return false;
+        }
+
+        // Running Binary Search to find the element
+        int row = i;
+        int start = 0;
+        int end = j;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+
+            if (target < matrix[row][mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3));
+        System.out.println(searchMatrix(new int[][]{{1}}, 0));
+    }
+}
---
+++ src/dsa/matrix/SearchA2DMatrixII.java
@@ -0,0 +1,22 @@
+package dsa.matrix;
+
+public class SearchA2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+
+        while (i >= 0 && i < matrix.length - 1 && j >= 0 && j <= matrix[i].length) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+
+            if (target < matrix[i][j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+}
