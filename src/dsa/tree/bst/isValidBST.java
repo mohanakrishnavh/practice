b4ae487eb1b91fc61e1649b04d53afb565a64c53
Mohanakrishna <mohanakrishnavh@gmail.com>
2023-09-19 11:35:09 -0700

    BST

--- src/dsa/tree/bst/isValidBST.java
+++ src/dsa/tree/bst/isValidBST.java
@@ -1,5 +1,17 @@
 package dsa.tree.bst;
 
-public class isValidBST {
+import dsa.tree.TreeNode;
 
+public class isValidBST {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+
+        return min < root.val && root.val < max && isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
 }
